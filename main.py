ac73a84a6a84add01cfab9c22860e751e851aa40
Ashton George <ashtongeorge17@gmail.com>
2025-07-13 20:11:08 -0400

    orchestrator + gitignore oauth token+credentials

--- main.py
+++ main.py
@@ -1,60 +1,23 @@
+from datetime import datetime
 from autogen_agentchat.agents import AssistantAgent
 from autogen_agentchat.ui import Console
 from autogen_ext.models.openai import OpenAIChatCompletionClient
-import asyncio
 from dotenv import load_dotenv
-import os
 
-from agents.weather import weather_agent 
+from agents.tasks import task_agent
+from agents.news import news_agent
+from agents.weather import weather_agent
+from agents.mailer import mailer_agent
 
 load_dotenv()
 
-# Define a model client. You can use other model client that implements
-# the `ChatCompletionClient` interface.
-model_client = OpenAIChatCompletionClient(
-    model="gpt-4o",
-    api_key=os.getenv('OPENAI_API_KEY'),
-)
-
-
-# Define a simple function tool that the agent can use.
-# For this example, we use a fake weather tool for demonstration purposes.
-# async def get_weather(city: str) -> str:
-#     """Get the weather for a given city."""
-#     return f"The weather in {city} is 73 degrees and Sunny."
-
-
-# Define an AssistantAgent with the model, tool, system message, and reflection enabled.
-# The system message instructs the agent via natural language.
-# agent = AssistantAgent(
-#     name="weather_agent",
-#     model_client=model_client,
-#     tools=[get_weather],
-#     system_message="You are a helpful assistant.",
-#     reflect_on_tool_use=True,
-#     model_client_stream=True,  # Enable streaming tokens from the model client.
-# )
-
-main_agent = AssistantAgent(
-    name="MorningOrchestrator",
-    model_client=model_client,
-    tools= [
-        {
-            "name": "weather_report",
-            "description:": "Ask the weather agent for the current and forecasted weather for today.",
-            "function": lambda: weather_agent.run_stream(task="Get the weather for today in Bedford, Loretto, and State College.")
-        }
-    ]
-)
-
-
-# Run the agent and stream the messages to the console.
 async def main() -> None:
-    await Console(main_agent.run_stream(task="What is the weather in New York?"))
-    # Close the connection to the model client.
-    await model_client.close()
+    date = datetime.now()
+    formattedDate = date.strftime("%A, %B %d, %Y at %I:%M %p")
+    weather_summary = await weather_agent.run(task=f"Summarize the weather in Bedford, Loretto, and State College for today, {formattedDate}, please. Be sure to include times of precipitation where applicable.")
+    task_summary = await task_agent.run(task=f"Outline my todo tasks please.")
+    news_summary = await news_agent.run(task=f"Summarize the news for today, {formattedDate}, please.")
+    await mailer_agent.run(task=f"Please send my morning report. Use the date, {formattedDate}, and following pieces of data to create the subject and body: Weather - {weather_summary}, News - {news_summary}, Tasks - {task_summary}")
 
-
-# NOTE: if running this inside a Python script you'll need to use asyncio.run(main()).
-# await main()
-asyncio.run(main())
+if __name__ == "__main__":
+    main()
